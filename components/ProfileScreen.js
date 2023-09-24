e47b69d6b0708d7762372d6dbd65efb2f4112c09
anliel21 <90245122+anliel21@users.noreply.github.com>
2023-09-24 15:30:17 +0200

    navbar in ProfileScreen

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,52 +1,63 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, { useState } from 'react'; // Added useState
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { initializeApp } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
+// Import the NavigationBar component
+import NavigationBar from './NavigationBar';
 
+function ProfileScreen() {
+  const [isUserMode, setIsUserMode] = useState(false); // Added state for user mode
+  const auth = getAuth();
 
-function ProfileScreen () {
+  // handleLogout håndterer log ud af en aktiv bruger.
+  // Metoden er en prædefineret metode, som firebase stiller til rådighed.  https://firebase.google.com/docs/auth/web/password-auth#next_steps
+  // Metoden er et asynkront kald.
+  const handleLogOut = async () => {
+    await signOut(auth).then(() => {
+      // Log ud lykkedes.
+    }).catch((error) => {
+      // Der opstod en fejl.
+    });
+  };
 
-    const auth = getAuth();
-    const user = auth.currentUser
-    //handleLogout håndterer log ud af en aktiv bruger.
-    //Metoden er en prædefineret metode, som firebase stiller tilrådighed  https://firebase.google.com/docs/auth/web/password-auth#next_steps
-    //Metoden er et asynkrontkald.
-    const handleLogOut = async () => {
-        await signOut(auth).then(() => {
-            // Sign-out successful.
-          }).catch((error) => {
-            // An error happened.
-          });
-    };
-
-    //Hvis der af en eller anden grund ikke skulle være muligt at fremfinde den aktive bruger,
-    //skal der udprintes en besked om dette igennem en tekstkomponent
-    if (!auth.currentUser) {
-        return <View><Text>Not found</Text></View>;
-    }
-
-    //I return() udnyttes en prædefineret metode, som firebase stiller til rådighed.
-    // Metoden returnerer mailadressen af den aktive bruger.
-    // Mailadressen udskrives ved brug af en tekstkomponent.
+  // Hvis der af en eller anden grund ikke skulle være muligt at fremfinde den aktive bruger,
+  // skal der udprintes en besked om dette igennem en tekstkomponent
+  if (!isUserMode) { // Use isUserMode state to toggle between user and development mode
     return (
-        <View style={styles.container} >
-            <Text>Current user: {user.email}</Text>
-            <Button onPress={() => handleLogOut()} title="Log out" />
-        </View>
+      <View style={styles.container}>
+        <Text>Development Mode: Not logged in</Text>
+        <Button onPress={() => setIsUserMode(true)} title="Switch to User Mode" />
+      </View>
     );
+  }
 
+  // If you are in user mode, the following code will execute
+  const user = auth.currentUser;
+
+  //I return() udnyttes en prædefineret metode, som firebase stiller til rådighed.
+  // Metoden returnerer mailadressen af den aktive bruger.
+  // Mailadressen udskrives ved brug af en tekstkomponent.
+  return (
+    <View style={styles.container}>
+      <Text>Current user: {user ? user.email : 'Not found'}</Text> {/* Check if user is available */}
+      <Button onPress={() => handleLogOut()} title="Log out" />
+
+      {/* Include the NavigationBar component at the bottom of the screen */}
+      <NavigationBar />
+    </View>
+  );
 }
 
-//Lokal styling til brug i ProfileScreen
+// Lokal styling til brug i ProfileScreen
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingTop: '5%',
-        backgroundColor: '#ecf0f1',
-        padding: 8,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: '5%',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
 });
 
-//Eksport af Loginform, således denne kan importeres og benyttes i andre komponenter
-export default ProfileScreen
+//Eksport af ProfileScreen, således denne kan importeres og benyttes i andre komponenter
+export default ProfileScreen;
