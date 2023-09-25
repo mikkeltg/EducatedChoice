7f70f287d6f2ce954f9a205bb7f74436f16d987e
emma98a <eman21ac@student.cbs.dk>
2023-09-25 18:23:39 +0200

    Settings, FAQ knap, FAQ screen, Om os knap, Om os screen

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //Importere Firebase Services
 import { getApps, initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
 
 //Importere vores componenter fra components mappe
-import ProfileScreen from './components/ProfileScreen';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
-
-
+import ProfileScreen from "./components/ProfileScreen";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+import SuggestionsScreen from "./components/SuggestionsScreen";
+import SettingsScreen from "./components/SettingsScreen";
+import NavigationBar from "./components/search_component";
+import SearchScreen from "./components/SearchScreen";
+import FAQScreen from "./components/FAQScreen";
+import AboutUsScreen from "./components/AboutUsScreen";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJ5ev4aFTWGFI5kqWXVtMcvhLgylXBL6Q",
@@ -22,10 +28,12 @@ const firebaseConfig = {
   storageBucket: "educated-choice.appspot.com",
   messagingSenderId: "416843846158",
   appId: "1:416843846158:web:20fd7a2ab2428c8017225f",
-  measurementId: "G-ELSS69HT2K"
+  measurementId: "G-ELSS69HT2K",
 };
 
 // Initialize Firebase
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [user, setUser] = useState({ loggedIn: false });
@@ -37,7 +45,7 @@ export default function App() {
   } else {
     console.log("Firebase not on!");
   }
- 
+
   const auth = getAuth();
 
   function onAuthStateChange(callback) {
@@ -46,18 +54,18 @@ export default function App() {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
-        callback({loggedIn: true, user: user});
+        callback({ loggedIn: true, user: user });
         console.log("You are logged in!");
         // ...
       } else {
         // User is signed out
         // ...
-        callback({loggedIn: false});
+        callback({ loggedIn: false });
       }
     });
   }
 
- //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
+  // Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
   useEffect(() => {
     const unsubscribe = onAuthStateChange(setUser);
     return () => {
@@ -65,44 +73,81 @@ export default function App() {
     };
   }, []);
 
-  //Her oprettes gæstekomponentsindhold, der udgøres af sign-up og login siderne
   const GuestPage = () => {
-    return(
-        <View style={styles.container}>
-          <Text style={styles.paragraph}>
-            Opret eller Login med din firebase Email
-          </Text>
-          
-          <Card style={{padding:20, margin: 20}}>
-            <SignUpForm />
-          </Card>
-          
-          <Card style={{padding:20, margin: 20}}>
-            <LoginForm />
-          </Card>
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Opret eller Login med din firebase Email
+        </Text>
 
-        </View>
-    )
-  }
+        <Card style={{ padding: 20, margin: 20 }}>
+          <SignUpForm />
+        </Card>
 
+        <Card style={{ padding: 20, margin: 20 }}>
+          <LoginForm />
+        </Card>
+      </View>
+    );
+  };
 
+  return ( // Heri returnerer vi vores NavigationContainer, som indeholder vores Stack.Navigator, som indeholder vores Stack.Screen komponenter, her kan navigeres mellem alle vores views
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={GuestPage}
+          options={{ title: "Guest Page" }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: "Profile" }}
+        />
+        <Stack.Screen
+          name="Suggestions"
+          component={SuggestionsScreen}
+          options={{ title: "Suggestions" }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ title: "Search" }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: "Settings" }}
+        />
+        <Stack.Screen
+          name="FAQ"
+          component={FAQScreen}
+          options={{ title: "Ofte stillede spørgsmål" }}
+        />
+        <Stack.Screen
+          name="AboutUs"
+          component={AboutUsScreen}
+          options={{ title: "Om os" }}
+        />
 
-
-  return user.loggedIn ? <ProfileScreen /> : <GuestPage/> ;
-
+        {/* Tilføjer flere skærme her */}
+      </Stack.Navigator>
+      <NavigationBar />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
