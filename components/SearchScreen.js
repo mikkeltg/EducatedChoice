633340912f1e563a27d8fe6e8c027a4f0fad4dd0
emma98a <eman21ac@student.cbs.dk>
2023-10-08 10:23:57 +0200

    ændret i searchscreen, efter merge

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,15 +1,3 @@
-// SearchScreen.js
-import React from 'react';
-import { View, Text } from 'react-native';
-
-function SearchScreen() {
-  return (
-    <View>
-      <Text>This is the Search screen</Text>
-      {/* Indsæt indhold for din søgeskærm her */}
-    </View>
-  );
-}
 import React from 'react';
 import { View, Text } from 'react-native';
 import SearchBar from './SearchBar';
