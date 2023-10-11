7ef6e062bee23c574dfce09c0b79e9fc38a39fb4
anliel21 <90245122+anliel21@users.noreply.github.com>
2023-10-11 15:25:17 +0200

    Drawer navigator implementeret

---
+++ components/Navigator.js
@@ -0,0 +1,27 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import ProfileScreen from "./screens/ProfileScreen";
+import FilterLocation from "./screens/FilterLocation";
+
+const Drawer = createDrawerNavigator();
+
+function Navigator() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Welcome">
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+        <Drawer.Screen name="Filter" component={FilterLocation} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
+
+//How to Navigator:
+//Importere jeres komponente overst
+//Sæt den ind i Drawer.Navigator: <Drawer.Screen name="TITEL PÅ SIDEN" component={SELVE NAVNET PÅ KOMPONENTEN} />
+//husk at eksportere jeres komponent i komponenten.js filen
--- components/login/WelcomeScreen.js
+++ components/login/WelcomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 
-const WelcomeScreen = ({ navigation }) => {
+const WelcomeScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Button
