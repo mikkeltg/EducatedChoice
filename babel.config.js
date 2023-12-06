10590c9bcdfd4a240a3d26b894ab0086ff6e9626
Snildsnozz <sillerishojjensen@hotmail.com>
2023-12-06 10:44:43 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,21 @@
+<<<<<<< Updated upstream
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: ['react-native-reanimated/plugin'],
 };
+=======
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: ['react-native-reanimated/plugin'],
+//   };
+
+  module.exports = function(api) {
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        'react-native-reanimated/plugin', // Tilføj dette plugin
+      ],
+    };
+  };
+>>>>>>> Stashed changes
