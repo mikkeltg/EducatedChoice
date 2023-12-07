f7fb419ed6b19e54f5b61076e188141d096ae407
Mikkel Teglers Gjørtsvang <migj21ab@student.cbs.dk>
2023-12-07 11:42:41 +0100

    Fungerende filterside

--- components/screens/Educations.js
+++ components/screens/Educations.js
@@ -37,27 +37,83 @@ function FilterEducationType({ navigation }) {
   const locations = ["København", "Aalborg", "Odense", "Aarhus", "Roskilde"];
   
   const [data, setData] = useState([]); // Gemmer uddannelser, der stemmer overens med filter og er hentet fra databasen
-  const [selectedAverage, setSelectedAverage] = useState(0); // Gemmer brugerens valg af adgangskvotient/gennemsnit 
+  const [filteredEducations, setfilteredEducations] = useState([]); // Gemmer uddannelser, der stemmer overens med filter og er hentet fra databasen
+  const [selectedAverage, setSelectedAverage] = useState(13); // Gemmer brugerens valg af adgangskvotient/gennemsnit 
   const [userAverage, setUserAverage] = useState(); // Gemmer brugerens valg af adgangskvotient/gennemsnit 
   const [selectedEducationType, setSelectedEducationType] = useState(""); // Gemmer brugerens valg af uddannelsestype 
   const [isModalVisible, setModalVisible] = useState(false); // Viser/gemmer dropdown bar til filtreringsmuligheder
   const [selectedLocation, setSelectedLocation] = useState();
+  const [selectedMinStartWage, setSelectedMinStartWage] = useState(0);
   
+  const getEligbleEducations = () => {
+    const eligibleEducations = [];
+    for (let i = 0; i < data.length; i++) {   
+      let educationEligibility = true; 
+      switch (true) { // Led efter parameter, der ikke stemmer overens med filter på type og løn
+        case selectedMinStartWage > data[i]["Løn"]:
+          educationEligibility = false;
+          continue;
+        case selectedEducationType.length > 0 && !data[i][selectedEducationType]:
+          educationEligibility = false;
+          continue;
+      };
 
+      const entryReqGrades = [];
+      let eligbleCity = false
+      for (let city in data[i]["Lokationer"]){
+        console.log(selectedLocation);
+        console.log(city);
+        data[i]["Lokationer"][city.trim()] = data[i]["Lokationer"][city];
+        city = city.trim(); // fjern evt. ekstra mellemrum
+        if (typeof selectedLocation != "undefined" && selectedLocation != city) {
+          console.log("ikke match på by");
+        } else {
+          console.log("match på by");
+          eligbleCity = true;
+          console.log(data[i]["Lokationer"][city]);
+          
+          for (const university in data[i]["Lokationer"][city]){
+            console.log(university);
+            if (data[i]["Lokationer"][city][university]["Adgangskvotient"] == "AO") {
+              data[i]["Lokationer"][city][university]["Adgangskvotient"] = 0;
+            }
+            entryReqGrades.push(data[i]["Lokationer"][city][university]["Adgangskvotient"])
+          }
+          }
+      }
+      entryReqGrades.sort((a, b) => a - b); // Sorter med laveste først
+      console.log(data[i]);
+      console.log(entryReqGrades);
+      console.log(selectedAverage);
+      console.log("Average: ", selectedAverage > entryReqGrades[0]);
+      console.log("City: ", !eligbleCity);
+      if (!eligbleCity) {
+        console.log("ikke match på nogen byer");
+        educationEligibility = false;
+      }
+      if (selectedAverage < entryReqGrades[0]) {
+        console.log("ikke match på karaktergennemsnit");
+        educationEligibility = false;
+      }
+
+     
+    
+      if(educationEligibility){
+        eligibleEducations.push(data[i]);
+      };
+    };
+    return eligibleEducations;
+  };
 
   // UseEffect der indhenter uddannelsesdata fra databasen efter brugeren har taget et valg vedrørende uddannelsestype 
   useEffect(() => {
     const fetchData = async () => { // Selve funktionen der indhenter uddannelsesdate fra databasen
       try {
         console.log("Fetching data for education type:", selectedEducationType);
-        if (selectedEducationType) {
+        if (data.length < 1) {
           const db = getFirestore();
           const uddannelserRef = collection(db, "Uddannelser");
-          const q = query(
-            uddannelserRef,
-            where(`${selectedEducationType}`, "==", true)
-          );
-          const querySnapshot = await getDocs(q);
+          const querySnapshot = await getDocs(uddannelserRef);
 
           const newData = [];
           querySnapshot.forEach((doc) => {
@@ -66,17 +122,14 @@ function FilterEducationType({ navigation }) {
 
           console.log("Fetched data:", newData);
           setData(newData);
-        } else {
-          console.log("No location selected. Clearing data.");
-          setData([]);
+          setfilteredEducations(newData)
         }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  }, [isModalVisible]);
-
+  }, []);
   // UseEffect der indhenter brugerens gennemsnit, hvis denne har en 
   const fetchUserGrades = async () => { // Selve funktionen der indhenter uddannelsesdate fra databasen
     try {
@@ -108,49 +161,21 @@ const useUsersOwnAverage = async () => {
   };
 
 
-  // Funktion til at fjerne brugerens filtervalg
-  const clearEducationTypeFilter = () => {
-    setSelectedEducationType(""); 
-  };
-
   // Nedenstående kode viser filtreringsbaren til brugeren samt en liste over de uddannelser, som lever op til den valgte filtrering
   return (
     <SafeAreaView style={GlobalStyles.containerL}>
-      <TouchableOpacity
-        onPress={toggleModal}
-        style={GlobalStyles.dropdownContainer}
-      >
+    <ScrollView>
+        <TouchableOpacity
+            onPress={toggleModal}
+            style={GlobalStyles.dropdownContainer}
+        >
         <Text style={GlobalStyles.dropdownInput}>Vælg filtre</Text>
         <Text style={GlobalStyles.dropdownArrow}>▼</Text>
-      </TouchableOpacity>
-      {/* Dropdown filter bar, der foldes ud, når brugeren trykker */}
-      <Modal visible={isModalVisible} animationType="slide">
-        <SafeAreaView style={GlobalStyles.container}>
+        </TouchableOpacity>
+        {/* Dropdown filter bar, der foldes ud, når brugeren trykker */}
+        <Modal visible={isModalVisible} animationType="slide">
+            <SafeAreaView style={GlobalStyles.container}>
             <Text style={GlobalStyles.headerL}>Vælg filtre</Text>
-            <View style={GlobalStyles.modalContainerL}>
-                <Text style={GlobalStyles.modalTitle}>Adgangskvotient</Text>
-                <View style={GlobalStyles.modalContainerLHorizontal}>
-                    <TextInput  
-                        style={GlobalStyles.numberInput}
-                        onChangeText={setSelectedAverage}
-                        value={selectedAverage}
-                        placeholder="Indsæt tal"
-                        keyboardType="numeric"
-                    />
-                <View style={GlobalStyles.containerGap}>
-                    {selectedAverage <= 0 ? (
-                        <Pressable style={GlobalStyles.smallButton} onPress={useUsersOwnAverage}>
-                        <Text style={GlobalStyles.text}>Brug mit karaktergennemsnit som filter</Text>
-                        </Pressable>
-                    ) : (
-                        <Pressable style={GlobalStyles.smallButton} onPress={() => setSelectedAverage(0)}>
-                        <Text style={GlobalStyles.text}>Fjern filter på adgangskvotient</Text>
-                        </Pressable>
-                    )}
-                </View>
-            </View>
-                {selectedAverage>0?<Text style={GlobalStyles.text}>Filter på agangskvotient er sat til: <Text style={[{fontWeight:"bold"}]}>{selectedAverage}</Text></Text>:null} 
-            </View>
             <View style={GlobalStyles.modalContainerL}>
                 <Text style={GlobalStyles.modalTitle}>Uddannelsestype</Text>
                 <FlatList
@@ -183,17 +208,62 @@ const useUsersOwnAverage = async () => {
                     extraData={selectedEducationType}
                 />
             </View>
-             <Pressable style={GlobalStyles.button} onPress={toggleModal}> 
+            <View style={GlobalStyles.modalContainerL}>
+                <Text style={GlobalStyles.modalTitle}>Adgangskvotient</Text>
+                <View style={GlobalStyles.modalContainerLHorizontal}>
+                    <TextInput  
+                        style={GlobalStyles.numberInput}
+                        onChangeText={setSelectedAverage}
+                        value={selectedAverage}
+                        placeholder="Indsæt tal"
+                        keyboardType="numeric"
+                    />
+                <View style={GlobalStyles.containerGap}>
+                    {selectedAverage != userAverage ? (
+                        <Pressable style={GlobalStyles.smallButton} onPress={useUsersOwnAverage}>
+                        <Text style={GlobalStyles.text}>Brug mit karaktergennemsnit som filter</Text>
+                        </Pressable>
+                    ) : (
+                        <Pressable style={GlobalStyles.smallButton} onPress={() => setSelectedAverage(13)}>
+                        <Text style={GlobalStyles.text}>Fjern filter på adgangskvotient</Text>
+                        </Pressable>
+                    )}
+                </View>
+            </View>
+                {selectedAverage!=13?<Text style={GlobalStyles.text}>Kun agangskvotient på <Text style={[{fontWeight:"bold"}]}>{selectedAverage}</Text> eller mindre</Text>:null} 
+            </View>
+            <View style={GlobalStyles.modalContainerL}>
+                <Text style={GlobalStyles.modalTitle}>Startløn</Text>
+                <View style={GlobalStyles.modalContainerLHorizontal}>
+                    <TextInput  
+                        style={GlobalStyles.numberInput}
+                        onChangeText={setSelectedMinStartWage}
+                        value={selectedMinStartWage}
+                        placeholder="Indsæt minimum startløn"
+                        keyboardType="numeric"
+                    />
+                <View style={GlobalStyles.containerGap}>
+                        <Pressable style={GlobalStyles.smallButton} onPress={() => setSelectedMinStartWage(0)}>
+                        <Text style={GlobalStyles.text}>Fjern filter på startløn</Text>
+                        </Pressable>
+                </View>
+            </View>
+                {selectedMinStartWage>0?<Text style={GlobalStyles.text}>Filter på startløn er sat til: <Text style={[{fontWeight:"bold"}]}>{selectedMinStartWage}</Text></Text>:null} 
+            </View>
+             <Pressable style={GlobalStyles.button} onPress={() => {
+                setfilteredEducations(getEligbleEducations());
+                toggleModal();
+              }}> 
                 <Text>Vælg filtre</Text>
             </Pressable>
         </SafeAreaView>
-      </Modal>
-      <Text style={[GlobalStyles.textL, {fontWeight: 'bold'}]}>
-      Viser følgende uddannelsestype: {String(selectedEducationType)}
+    </Modal>
+    <Text style={[GlobalStyles.textL, {fontWeight: 'bold'}]}>
+    Viser uddannelser for følgende filtre: {String(selectedEducationType)} {selectedAverage != 13 ? selectedAverage: ""} {selectedLocation} {selectedMinStartWage > 0 ? selectedMinStartWage: ""}
     </Text>
     <ScrollView>
     {/* Her vises de enkelte uddannelser */}
-    {data.map((document, index) => (
+    {filteredEducations.map((document, index) => (
         <View key={index} style={GlobalStyles.documentContainerL}>
             <Text style={GlobalStyles.headerL}>{document.Navn}</Text>
             <Button
@@ -205,9 +275,15 @@ const useUsersOwnAverage = async () => {
     ))}
     </ScrollView>
       {/* Nedenstående gør det muligt at nulstille filter */}
-      <Pressable style={GlobalStyles.button} onPress={clearEducationTypeFilter}> 
+      <Pressable style={GlobalStyles.button} onPress={() => {
+        setSelectedAverage(13);
+        setSelectedMinStartWage(0);
+        setSelectedEducationType("");
+        setSelectedLocation();
+      }}> 
         <Text>Clear Filter</Text>
       </Pressable>
+    </ScrollView>
     </SafeAreaView>
   );
 }
