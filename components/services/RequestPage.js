c005990cb3282e2a136a80dc07b2087790ebded1
emma98a <eman21ac@student.cbs.dk>
2023-12-19 10:22:04 +0100

    finetunede præ-træning til robot + erstattet api key

--- components/services/RequestPage.js
+++ components/services/RequestPage.js
@@ -1,52 +1,52 @@
-//Sample import fra https://rapidapi.com/rphrp1985/api/open-ai21
-// Hunsk at insætte den sample de giver in i en function som jeg har gjort i lijne 5, dernæst ændre content: "hello" til en variable (message her), Husk også at addere en return response
-import axios from "axios";
-import { CHATMESSAGES } from "./const";
+ //Sample import fra https://rapidapi.com/rphrp1985/api/open-ai21
+  // Hunsk at insætte den sample de giver in i en function som jeg har gjort i lijne 5, dernæst ændre content: "hello" til en variable (message her), Husk også at addere en return response
+  import axios from "axios";
+  import { CHATMESSAGES } from "./const";
 
-export default async function SendMessage(message) { //kun en bruger kan sende en besked 
-  //pusher brugerens besked til CHATMESSAGES array
-  const toChatMessagesUSER = {
-    role: 'user',
-    content: message
-  }
-  CHATMESSAGES.push(toChatMessagesUSER);
-
-  //DEFINERE VORES OPTIONS TIL API KALD
-  const options = {
-    method: 'POST',
-    url: 'https://open-ai21.p.rapidapi.com/conversationgpt35',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'de425c7196msh76ea8e574de06fbp1fbaf9jsn658f09cbca4b',
-      'X-RapidAPI-Host': 'open-ai21.p.rapidapi.com',
-    },
-    data: {
-      messages: CHATMESSAGES, //HER SENDER VI VORES CHATMESSAGES ARRAY MED ALLE SPØRGSMÅL OG SVAR
-      web_access: false,
-      stream: false,
-    },
-  };
-
-  try {
-    const response = await axios.request(options); //HER SENDER VI REQUEST AFSTED TIL API'EN
-    console.log(response.data);
-    //pusher AI svaret fra api'en til CHATMESSAGES array
-    const toChatMessages = {
-      role: 'assistant',
-      content: response
+  export default async function SendMessage(message) { //kun en bruger kan sende en besked 
+    //pusher brugerens besked til CHATMESSAGES array
+    const toChatMessagesUSER = {
+      role: 'user',
+      content: message
     }
-    CHATMESSAGES.push(toChatMessages); //opdatere vores CHATMESSAGES array med den nye besked
-    //returner svar til brugeren
-    return response
-  } catch (error) {
-    console.error('API Request Error:', error.message);
-    if (error.response) {
-      console.error('Server Response:', error.response.data);
-      console.error('Status Code:', error.response.status);
-    } else if (error.request) {
-      console.error('No response received from the server');
-    } else {
-      console.error('Error configuring the request:', error.message);
+    CHATMESSAGES.push(toChatMessagesUSER);
+
+    //DEFINERE VORES OPTIONS TIL API KALD
+    const options = {
+      method: 'POST',
+      url: 'https://open-ai21.p.rapidapi.com/conversationgpt35',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '4c4836105dmsh0904283da9b43dfp14128fjsn7563ba9da4cd',
+        'X-RapidAPI-Host': 'open-ai21.p.rapidapi.com',
+      },
+      data: {
+        messages: CHATMESSAGES, //HER SENDER VI VORES CHATMESSAGES ARRAY MED ALLE SPØRGSMÅL OG SVAR
+        web_access: false,
+        stream: false,
+      },
+    };
+
+    try {
+      const response = await axios.request(options); //HER SENDER VI REQUEST AFSTED TIL API'EN
+      console.log(response.data);
+      //pusher AI svaret fra api'en til CHATMESSAGES array
+      const toChatMessages = {
+        role: 'assistant',
+        content: response
+      }
+      CHATMESSAGES.push(toChatMessages); //opdatere vores CHATMESSAGES array med den nye besked
+      //returner svar til brugeren
+      return response
+    } catch (error) {
+      console.error('API Request Error:', error.message);
+      if (error.response) {
+        console.error('Server Response:', error.response.data);
+        console.error('Status Code:', error.response.status);
+      } else if (error.request) {
+        console.error('No response received from the server');
+      } else {
+        console.error('Error configuring the request:', error.message);
+      }
     }
   }
-}
--- components/services/const.js
+++ components/services/const.js
@@ -1,29 +1,47 @@
 export const CHATMESSAGES = [
-    {
-      role: "system", //SKAL VÆRE SYSTEM HVIS DET ER SYSTEM BESKEDER
-      content: "Du er en AI, og du skal besvare spørgsmål for folk der skal træffe valg om bachelor eller kandidat uddannelse i Danmark. Du skal bl.a. hjælpe dem med at besvare spørgsmål om hvilke mulige jobstitler der er, efter endt uddannelse. Herudover skal du også besvare deres spørgsmål omkring hvordan startlønnen er efter den endt uddannelse de spørger om. Du må gerne svare på en venlig og professionelt måde. Du skal helst svare på dansk, men hvis det er helt umuligt at give et fydelstgørende svar, må du gerne svare på engelsk. ", //SKAL VÆRE EKSTREMT DETALJERET OG SKÅRET I PAP - 3-5 linjer - forklar hvad der er tilladt og ikke tilladt - "du er en ai, du må gerne besvare dette og ikke svare på dette"
-    },
-    {
-      role: "user",
-      content: "Hvad man kan blive som færdiguddannet biolog?", //komme med et eksempel på et spørgsmål
-    },
-    {
-      role: "assistant",
-      content: "Biolog:  Almindelig betegnelse for en person med en biologiuddannelse, der kan arbejde inden for forskellige områder.  Økolog: Specialiseret i studiet af interaktioner mellem organismer og deres miljø. Mikrobiolog: Specialiseret i studiet af mikroorganismer som bakterier, vira og svampe.  Genetiker: Studerer gener og genetisk materiale for at forstå arvelige egenskaber og sygdomme.  Marinbiolog: Specialiseret i studiet af marine organismer og deres økosystemer.   Botaniker: Studerer planter, herunder deres struktur, funktion og økologi. Zoolog: Fokuserer på studiet af dyr, herunder deres adfærd, fysiologi og evolution.  Miljøkonsulent: Arbejder med at evaluere og rådgive om miljømæssige påvirkninger af forskellige aktiviteter.  Feltbiolog:  Arbejder typisk udendørs i marken og indsamler data om naturlige habitater og organismer.  Biomedicinsk forsker:  Arbejder inden for medicinsk forskning og undersøger biologiske processer relateret til sundhed og sygdomme.  Konservator: Arbejder med at bevare og beskytte truede arter og deres levesteder.  Biostatistiker:   Anvender statistiske metoder til at analysere biologiske data og udlede meningsfulde resultater. Forskningstekniker:  Assisterer i laboratorie- eller feltarbejde inden for forskningsprojekter.  Underviser eller forsker ved universitetet:  Arbejder inden for uddannelse og/eller forskning på et universitet eller lignende institution. Disse jobtitler dækker et bredt spektrum af karrieremuligheder for færdiguddannede biologer, og mange fagfolk kan have tværfaglige roller, der kombinerer flere aspekter af biologi og relaterede felter.", //komme med et eksempel på et ønsket svar, som ai'en skal svare
-    }, //desto flere eksempler desto bedre, men for mange eksempler kan gøre det svært for ai'en at svare
-    {
-      role:"user",
-      content: "Hvad er startlønnen for en biolog?", //komme med et eksempel på et spørgsmål
-    },
   {
-    role:"assistant",
-    content: "Startlønnen for en biolog er 32.500 kr. om måneden ifølge Danmarks Statistik", //komme med et eksempel på et ønsket svar, som ai'en skal svare
-  }
-  ];
-  
-  
-  
-  //undersøg how to clear an array, hvis brugeren gerne vil starte på eb ny samtale - reset knap
-  //husk at lave ny APIkey lige inden vi afleverer  -sille og mikkel kan lige oprette keys ved siden af
-  //husk at lave en slide og ikke fremlægge om det samme
-  
+    role: "system", //SKAL VÆRE SYSTEM HVIS DET ER SYSTEM BESKEDER
+    content: "Du er en AI, og du skal besvare spørgsmål for folk der skal træffe valg om bachelor eller kandidat uddannelse i Danmark. Du skal bl.a. hjælpe dem med at besvare spørgsmål om hvilke mulige jobstitler der er, efter endt uddannelse. Herudover skal du også besvare deres spørgsmål omkring hvordan startlønnen er efter den endt uddannelse de spørger om. Du skal besvare spørgsmål om de enkelte universiteter, omkring campus og faciliteterne. Du skal svare med et glad og positivt svar på dansk. Hvis du ikke kan svare på spørgsmålet med et komplet svar på dansk, så skal du svare tilbage at de skal omformulere sig eller stille et nyt spørgsmål.", //SKAL VÆRE EKSTREMT DETALJERET OG SKÅRET I PAP - 3-5 linjer - forklar hvad der er tilladt og ikke tilladt - "du er en ai, du må gerne besvare dette og ikke svare på dette"
+  },
+  {
+    role: "user",
+    content: "Hvad man kan blive som færdiguddannet biolog?", //komme med et eksempel på et spørgsmål
+  },
+  {
+    role: "assistant",
+    content: "Biolog:  Almindelig betegnelse for en person med en biologiuddannelse, der kan arbejde inden for forskellige områder.  Økolog: Specialiseret i studiet af interaktioner mellem organismer og deres miljø. Mikrobiolog: Specialiseret i studiet af mikroorganismer som bakterier, vira og svampe.  Genetiker: Studerer gener og genetisk materiale for at forstå arvelige egenskaber og sygdomme.  Marinbiolog: Specialiseret i studiet af marine organismer og deres økosystemer.   Botaniker: Studerer planter, herunder deres struktur, funktion og økologi. Zoolog: Fokuserer på studiet af dyr, herunder deres adfærd, fysiologi og evolution.  Miljøkonsulent: Arbejder med at evaluere og rådgive om miljømæssige påvirkninger af forskellige aktiviteter.  Feltbiolog:  Arbejder typisk udendørs i marken og indsamler data om naturlige habitater og organismer.  Biomedicinsk forsker:  Arbejder inden for medicinsk forskning og undersøger biologiske processer relateret til sundhed og sygdomme.  Konservator: Arbejder med at bevare og beskytte truede arter og deres levesteder.  Biostatistiker:   Anvender statistiske metoder til at analysere biologiske data og udlede meningsfulde resultater. Forskningstekniker:  Assisterer i laboratorie- eller feltarbejde inden for forskningsprojekter.  Underviser eller forsker ved universitetet:  Arbejder inden for uddannelse og/eller forskning på et universitet eller lignende institution. Disse jobtitler dækker et bredt spektrum af karrieremuligheder for færdiguddannede biologer, og mange fagfolk kan have tværfaglige roller, der kombinerer flere aspekter af biologi og relaterede felter.", //komme med et eksempel på et ønsket svar, som ai'en skal svare
+  }, //desto flere eksempler desto bedre, men for mange eksempler kan gøre det svært for ai'en at svare
+  {
+    role:"user",
+    content: "Hvad er startlønnen for en biolog?", //komme med et eksempel på et spørgsmål
+  },
+{
+  role:"assistant",
+  content: "Startlønnen for en biolog er 32.500 kr. om måneden ifølge Danmarks Statistik", //komme med et eksempel på et ønsket svar, som ai'en skal svare
+},
+{
+  role:"user",
+  content: "Er der et bederum på CBS?", //komme med et eksempel på et spørgsmål
+},
+{
+role:"assistant",
+content: "På DTU og på Aarhus Universitet hedder lokalerne 'stillerum', på Aalborg Universitet og SDU er der 'fordybelsesrum', CBS har et 'andagtsrum' og på Københavns Universitet findes 'retræterum' og 'stillerum'. KU udmærker sig ved at have hele syv retræte- eller stillerum. De øvrige universiteter har typisk kun et enkelt.", //komme med et eksempel på et ønsket svar, som ai'en skal svare
+},
+{
+role:"user",
+content: "Hvis jeg gerne vil være håndværker, hvad skal jeg vælge af uddannelse?", //komme med et eksempel på et spørgsmål
+},
+{
+role:"assistant",
+content: "At vælge en uddannelse som håndværker indebærer normalt at vælge en faglig eller erhvervsuddannelse inden for et specifikt håndværksområde. Her er nogle af de mest almindelige håndværksuddannelser: Erfaringsbaseret læreplads (EUX), Erhvervsuddannelser (EUD), Teknisk gymnasium, Håndværkerhøjskole", //komme med et eksempel på et ønsket svar, som ai'en skal svare
+},
+{
+role:"user",
+content: "hvilken uddannelse på hvilket universitet skal jeg vælge hvis jeg gerne vil studere dyr?", //komme med et eksempel på et spørgsmål
+},
+{
+role:"assistant",
+content: "Hvis du er interesseret i at studere dyr, er der flere uddannelser og universiteter, der tilbyder relevante programmer. Her er nogle populære uddannelser og universiteter inden for dette område: Dyrlægeuddannelsen: En uddannelse som dyrlæge giver dig en omfattende forståelse af dyr, deres sundhed og sygdomme. Du kan overveje at ansøge om dyrlægestudiet ved universiteter, der tilbyder dette program. Eksempler inkluderer:Københavns Universitet, Danmark", //komme med et eksempel på et ønsket svar, som ai'en skal svare
+}
+
+];
